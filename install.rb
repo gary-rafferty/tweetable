e8926e52eb91c1d66f11f3a6109f69c107d4f92f
rlzy1r <rlzy1r@mebuntu.(none)>
2010-12-06 19:52:02 +0000

    Using puts instead of write

--- install.rb
+++ install.rb
@@ -1,8 +1,8 @@
 config_file = File.open("#{Rails.root}/config/twitter.yml",'w') do |file|
-  file.write("consumer_key: \"YOUR_CONSUMER_KEY\"")
-  file.write("consumer_secret: \"YOUR_CONSUMER_SECRET\"")
-  file.write("oauth_token: \"YOUR_OAUTH_TOKEN\"")
-  file.write("oauth_token_secret: \"YOUR_OAUTH_TOKEN_SECRET\"")
+  file.puts("consumer_key: \"YOUR_CONSUMER_KEY\"")
+  file.puts("consumer_secret: \"YOUR_CONSUMER_SECRET\"")
+  file.puts("oauth_token: \"YOUR_OAUTH_TOKEN\"")
+  file.puts("oauth_token_secret: \"YOUR_OAUTH_TOKEN_SECRET\"")
 end
 
 puts "\nPlugin Installed"
